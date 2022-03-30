f98a63541c6946c62b022ec8443092f8e228803d
Pedro <pedrodsm1819@gmail.com>
2022-03-29 22:24:11 -0300

    Inserindo o primeiro frame do Menu Inicial e criação dos objetos que representa cada tela

--- src/view/TelaMenu.java
+++ src/view/TelaMenu.java
@@ -18,25 +18,40 @@ import controle.*;
 public class TelaMenu implements ActionListener {
 		private static JFrame janela = new JFrame("Controle de Bicicletas Compartilhadas");
 		private static JLabel titulo = new JLabel("Menu Principal");
-		private static JButton bicicletas = new JButton("Bicicletas");
-		private static JButton posto = new JButton("Postos disponíveis");
-		private static JButton usuario = new JButton("Usuários");
+		private static JButton posto = new JButton("Postos");
+		private static JButton usuario = new JButton("Usuarios");
 	
 	public TelaMenu() {
 		titulo.setFont(new Font("Arial", Font.BOLD, 20));
 		titulo.setBounds(120, 10, 150, 30);
-		bicicletas.setBounds(140, 100, 100, 30);
+		usuario.setBounds(140, 100, 100, 30);
 		posto.setBounds(140, 50, 100, 30);
+
+		janela.setLayout(null);
+		
+		janela.add(titulo);
+		janela.add(posto);
+		janela.add(usuario);
+		
+		janela.setSize(400,250);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setVisible(true);
 	}
 		
 	public static void main(String[] args) {
-	
+		TelaMenu menu = new TelaMenu();
 			
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		Object src = e.getSource();
+		
+		// Seção que deve ser usada quando construir a TelaPosto, TelaUsuario e TelaPlanoAssinatura
+		
+//		if (src == posto) {
+//			new TelaPosto.mostrarDados();
+//		}
 		
 	}
 
